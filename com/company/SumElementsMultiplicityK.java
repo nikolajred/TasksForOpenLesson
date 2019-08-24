f3dddafb99832dfbea490b98f8613fff08a47962
nikolajred <nikolajred@gmail.com>
2019-08-24 11:09:05 +0300

    solved

---
+++ com/company/SumElementsMultiplicityK.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+/* Given an array of natural numbers. Find the sum of elements that are multiples of a given K */
+public class SumElementsMultiplicityK {
+private static int elementsOfArray;
+
+    public static int getElementsOfArray() {
+        return elementsOfArray;
+    }
+
+    public static void setElementsOfArray(int elementsOfArray) {
+        if(elementsOfArray >= 0) {
+            SumElementsMultiplicityK.elementsOfArray = elementsOfArray;
+        }else{
+            System.out.println("your number isn't integer or < 0");
+        }
+    }
+
+    private static int sumElementsMultiplicityK;
+    public static void main(String[] args) {
+        Scanner scn = new  Scanner(System.in);
+        System.out.println("Input number of elements of array:");
+        elementsOfArray = scn.nextInt();
+        int [] array = new int[elementsOfArray];
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int)((Math.random() * 100)/(Math.random() * 10));
+            System.out.print(array[i] + " ");
+        }
+        System.out.println(" ");
+        System.out.println("enter the number K to which the multiplicity is checked: ");
+        int K = scn.nextInt();
+        for(int i = 0; i < array.length; i++){
+            if (array[i]%K==0){
+                System.out.print(array[i] + " ");
+                sumElementsMultiplicityK += array[i];
+            }
+        }
+        System.out.println(" ");
+        System.out.println("sum of elements that are multiples of k: " + sumElementsMultiplicityK);
+    }
+}
