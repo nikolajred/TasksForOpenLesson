eabae1faf63fab2f6cc630517d7e5bd8d3b4135a
nikolajred <nikolajred@gmail.com>
2019-08-24 17:51:08 +0300

    solved fifth

---
+++ com/company/AreaOfRightTriangle.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class AreaOfRightTriangle {
+    public static int cathetA;
+    public static int cathetB;
+    public static double hypotenuse;
+    public static double pyrimeter;
+    public static int square;
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Input cathet a: ");
+        cathetA = scn.nextInt();
+        System.out.println("Input cathet b:");
+        cathetB = scn.nextInt();
+        hypotenuse = Math.hypot(cathetA, cathetB);
+        System.out.println("hypotenuse is: " + Math.round(hypotenuse*100)/100.00d);
+        pyrimeter = cathetA + cathetB + hypotenuse;
+        System.out.println("pyrimeter is: " + Math.round(pyrimeter*100)/100.00d);
+        square = cathetA * cathetB / 2;
+        System.out.println("square is: " + square);
+    }
+}
